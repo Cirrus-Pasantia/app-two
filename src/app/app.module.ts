f4ef170610d7f82cdbdf9e1c24fbbb1bcc3f24cc
Rodolfo Christian Catunta Uturunco <rodolfo.catunta.uturunco@gmail.com>
2021-05-26 11:50:36 -0400

    Solving Routing Problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [
     {provide: APP_BASE_HREF, useValue: '/'} // Add providers Manually
